72d46a1d21a2966bf3b032d527b3af6f915ef2b0
Cunhaaa <gabriel.dsilveira@al.infnet.edu.br>
2021-05-31 22:11:17 -0300

    add 'CPU' page

--- gerenciador_de_tarefas/views.py
+++ gerenciador_de_tarefas/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.template import loader
 import psutil
+import cpuinfo
 
 def formatar_memoria(valor):
     return round(valor/(1024*1024*1024), 2)
@@ -14,5 +15,15 @@ def index(request):
         'disco_em_uso': formatar_memoria(disco.used),
         'disco_livre': formatar_memoria(disco.free),
         'disco_percentual_usado': disco.percent,
+    }
+    return HttpResponse(template.render(context, request))
+
+def processo(request):
+    template = loader.get_template('processo.html')
+    context = {
+        'nome': "Gerenciador de tarefas",
+        'cpu_name': cpuinfo.get_cpu_info()['brand_raw'],
+        'nucleos_tot': psutil.cpu_count(),
+        'cpu_percent': psutil.cpu_percent(),
     }
     return HttpResponse(template.render(context, request))
