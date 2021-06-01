954621a67ff083bc50d1cc7863be8a7b5a2c10d4
Cunhaaa <gabriel.dsilveira@al.infnet.edu.br>
2021-06-01 12:08:23 -0300

    add memoria page

--- gerenciador_de_tarefas/urls.py
+++ gerenciador_de_tarefas/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('processo/', views.processo, name='processo'),
+    path('memoria/', views.memoria, name='memoria'),
 ]
--- gerenciador_de_tarefas/views.py
+++ gerenciador_de_tarefas/views.py
@@ -23,7 +23,23 @@ def processo(request):
     context = {
         'nome': "Gerenciador de tarefas",
         'cpu_name': cpuinfo.get_cpu_info()['brand_raw'],
-        'nucleos_tot': psutil.cpu_count(),
+        'archtecture': cpuinfo.get_cpu_info()['arch'],
+        'bits': cpuinfo.get_cpu_info()['bits'],
+        'nucleos_logic_tot': psutil.cpu_count(logical = True),
+        'nucleos_fisic_tot': psutil.cpu_count(logical = False),
         'cpu_percent': psutil.cpu_percent(),
     }
     return HttpResponse(template.render(context, request))
+
+def memoria(request):
+    memoria = psutil.swap_memory()
+    template = loader.get_template('memoria.html')
+    context = {
+        'nome': "Gerenciador de tarefas",
+        'tot_memory': formatar_memoria(memoria[0]),
+        'memory_used': formatar_memoria(memoria[1]),
+        'memory_free': formatar_memoria(memoria[2]),
+        'percent_memory_used': memoria[3],
+    }
+    return HttpResponse(template.render(context, request))
+
