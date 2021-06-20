24d79f0ee56a9fcef627383db3b128210d63d031
Cunhaaa <gabriel.dsilveira@al.infnet.edu.br>
2021-06-20 00:21:31 -0300

    mudança layout navbar, e adição das abas de arquivo e sub processos

--- gerenciador_de_tarefas/urls.py
+++ gerenciador_de_tarefas/urls.py
@@ -4,7 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('processo/', views.processo, name='processo'),
+    path('processo/', views.processador, name='processador'),
     path('memoria/', views.memoria, name='memoria'),
     path('rede/', views.rede, name='rede'),
+    path('arquivos/', views.arquivos, name='arquivos'),
+    path('sub_processos/', views.sub_processos, name='sub_processos'),
 ]
--- gerenciador_de_tarefas/views.py
+++ gerenciador_de_tarefas/views.py
@@ -4,25 +4,12 @@ import psutil
 import cpuinfo
 import os
 import time
+import subprocess
 
 def formatar(valor):
     return round(valor/(1024*1024*1024), 2)
 
-
-
-
 def index(request):
-    lista = os.listdir()
-    dic = {}
-    for i in lista:
-        if os.path.isfile(i):
-            dic[i] = []
-            dic[i].append(os.stat(i).st_size)
-            dic[i].append(os.stat(i).st_atime)
-            dic[i].append(os.stat(i).st_mtime)
-
-    
-    
     disco = psutil.disk_usage('.')
     template = loader.get_template('index.html')    
     context = {
@@ -30,12 +17,15 @@ def index(request):
         'disco_em_uso': formatar(disco.used),
         'disco_livre': formatar(disco.free),
         'disco_percentual_usado': disco.percent,
-        'arquivos': dic,
     }
     return HttpResponse(template.render(context, request))
 
-def processo(request):
-    template = loader.get_template('processo.html')
+def processador(request):
+    cores = []
+    for corePercent in psutil.cpu_percent(percpu=True):
+        cores.append(corePercent)
+
+    template = loader.get_template('processador.html')
     context = {
         'cpu_name': cpuinfo.get_cpu_info()['brand_raw'],
         'archtecture': cpuinfo.get_cpu_info()['arch'],
@@ -44,7 +34,7 @@ def processo(request):
         'freq_total': psutil.cpu_freq().max,
         'freq_uso': psutil.cpu_freq().current,
         'nucleos_fisic_tot': psutil.cpu_count(logical = False),
-        'cpu_percent': psutil.cpu_percent(),
+        'cpu_percent': cores,
     }
     return HttpResponse(template.render(context, request))
 
@@ -68,3 +58,48 @@ def rede(request):
     }
     return HttpResponse(template.render(context, request))
 
+def arquivos(request):
+    lista = os.listdir()
+    dic = {}
+    for i in lista:
+        if os.path.isfile(i):
+            dic[i] = []
+            dic[i].append(os.stat(i).st_size)
+            dic[i].append(os.stat(i).st_atime)
+            dic[i].append(os.stat(i).st_mtime)
+    template = loader.get_template('arquivos.html')
+    context = {
+        'arquivos': dic,
+    }
+    return HttpResponse(template.render(context, request))
+
+def sub_processos(request):
+    def mostra_info(pid):
+        try:
+            p = psutil.Process(pid)
+            texto = '{:6}'.format(pid)
+            texto = texto + '{:11}'.format(p.num_threads())
+            texto = texto + " " + time.ctime(p.create_time()) + " "
+            texto = texto + '{:8.2f}'.format(p.cpu_times().user)
+            texto = texto + '{:8.2f}'.format(p.cpu_times().system)
+            texto = texto + '{:10.2f}'.format(p.memory_percent()) + " MB"
+            rss = p.memory_info().rss/1024/1024
+            texto = texto + '{:10.2f}'.format(rss) + " MB"
+            vms = p.memory_info().vms/1024/1024
+            texto = texto + '{:10.2f}'.format(vms) + " MB"
+            texto = texto + " " + p.exe()
+            return texto
+        except:
+            pass  
+
+    lista = psutil.pids()
+    novaLista = []
+
+    for i in lista:
+        novaLista.append(mostra_info(i))
+
+    template = loader.get_template('sub_processos.html')
+    context = {
+        'processos': novaLista,
+    }
+    return HttpResponse(template.render(context, request))
